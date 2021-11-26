f68ca3c1c16a98ff423da5380cd385a5ff195c72
TKukharyuk <TKukharyuk@sportmaster.ru>
2021-11-26 15:15:06 +0300

    adprofiles-tk-ne добавил транспорт

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,21 @@
 rootProject.name = "adprofiles"
 
 pluginManagement{
-    val kotlinVersion: String by settings
 
     plugins {
+        val kotlinVersion: String by settings
+        val openApiVersion: String by settings
+        val bmuschkoVersion: String by settings
+
         kotlin("jvm") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+        kotlin("plugin.allopen") version kotlinVersion
+        id("org.openapi.generator") version openApiVersion
+        id("com.bmuschko.docker-java-application") version bmuschkoVersion
     }
 }
 
 
 include("common")
+include("adprofiles-transport-main-openapi")
